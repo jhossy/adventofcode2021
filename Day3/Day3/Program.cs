4191c3cd7033e9e2bc30b7902656e4f67879b979
Jesper Hossy <JEHOS@danskebank.dk>
2021-12-03 15:15:51 +0100

    added part2

--- Day3/Day3/Program.cs
+++ Day3/Day3/Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day3
 {
 	internal class Program
 	{
 		static void Main(string[] args)
+		{
+			//List<string> list = new List<string>() { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"};
+
+			FileParser fileParser = new FileParser();
+
+			List<string> list = fileParser.Parse("Input.txt");
+
+			SubmarineGenerator generator = new SubmarineGenerator();
+
+			var oxygenRaw = generator.SearchOxygen(list);
+			
+			int oxygen = Convert.ToInt32(oxygenRaw.First(), 2);
+
+			var co2Raw = generator.SearchC02(list);
+
+			int co2 = Convert.ToInt32(co2Raw.First(), 2);
+
+			Console.WriteLine($"oxygenRaw: {oxygenRaw[0]} - oxygen: {oxygen}");
+
+			Console.WriteLine($"co2Raw: {co2Raw[0]} - co2: {co2}");
+
+			Console.ReadLine();
+		}
+
+		static void Part1()
 		{
 			//List<string> list = new List<string>() { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"};
 
@@ -26,8 +52,6 @@ namespace Day3
 			Console.WriteLine($"GammaRaw: {gammaRaw} - EpsilonRaw: {epsilonRaw}");
 
 			Console.WriteLine($"Gamma: {gamma} - Epsilon: {epsilon}");
-
-			Console.ReadLine();
 		}
 	}
 }
---
+++ Day3/Day3/SubmarineGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day3
+{
+	public class SubmarineGenerator
+	{
+		public List<string> SearchOxygen(List<string> inputValues)
+		{
+			List<string> tempResult = inputValues;
+
+			int inputLength = inputValues.First().Length;
+
+			int noofZeros = 0;
+			int noOfOnes = 0;
+
+			for(int i=0; i < inputLength; i++)
+			{
+				if (tempResult.Count == 1) return tempResult;
+
+				foreach (string str in tempResult)
+				{
+					if(str[i] == '0')
+					{
+						noofZeros++;
+					}
+					else
+					{
+						noOfOnes++;
+					}
+				}
+
+				if(noOfOnes >= noofZeros)
+				{
+					tempResult = tempResult.Where(x => x[i] == '1').ToList();
+				}
+				else
+				{
+					tempResult = tempResult.Where(x => x[i] == '0').ToList();
+				}
+
+				noOfOnes = 0;
+				noofZeros = 0;
+			}
+
+			return tempResult;
+		}
+
+		public List<string> SearchC02(List<string> inputValues)
+		{
+			List<string> tempResult = inputValues;
+
+			int inputLength = inputValues.First().Length;
+
+			int noofZeros = 0;
+			int noOfOnes = 0;
+
+			for (int i = 0; i < inputLength; i++)
+			{
+				if (tempResult.Count == 1) return tempResult;
+
+				foreach (string str in tempResult)
+				{
+					if (str[i] == '0')
+					{
+						noofZeros++;
+					}
+					else
+					{
+						noOfOnes++;
+					}
+				}
+
+				if (noOfOnes >= noofZeros)
+				{
+					tempResult = tempResult.Where(x => x[i] == '0').ToList();
+				}
+				else
+				{
+					tempResult = tempResult.Where(x => x[i] == '1').ToList();
+				}
+
+				noOfOnes = 0;
+				noofZeros = 0;
+			}
+
+			return tempResult;
+		}
+	}
+}
