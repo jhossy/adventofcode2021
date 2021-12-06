477b60c441db84911842ce020e6acd762d6b11f4
Jesper Hossy <JEHOS@danskebank.dk>
2021-12-06 16:22:01 +0100

    updated day 6

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -10,8 +10,8 @@ namespace Day6
 			long[] allFish = new long[9];
 
 			FileParser fileParser = new FileParser();
-			//int[] initialStates = fileParser.Parse("TaskInput.txt");
-			int[] initialStates = fileParser.Parse("DemoInput.txt");
+			int[] initialStates = fileParser.Parse("TaskInput.txt");
+			//int[] initialStates = fileParser.Parse("DemoInput.txt");
 						
 			foreach (int state in initialStates)
 			{
@@ -19,10 +19,10 @@ namespace Day6
 				Console.WriteLine($"Added {state}");
 			}
 
-			List<long> resetFish = new List<long>();
-			for (int i = 1; i < 18; i++)
+			long resetFish = 0;
+			for (int i = 0; i < 256; i++)
 			{
-				if (allFish[0] > 0) resetFish.Add(allFish[0]);
+				resetFish = allFish[0];
 
 				//every day, the timer for each fish decrease by one, i.e. moving it around the array
 				allFish[0] = allFish[1];
@@ -31,17 +31,15 @@ namespace Day6
 				allFish[3] = allFish[4];
 				allFish[4] = allFish[5];
 				allFish[5] = allFish[6];
-				allFish[6] = resetFish.Count + allFish[7];
+				allFish[6] = resetFish + allFish[7];
 				allFish[7] = allFish[8];
-				allFish[8] = resetFish.Count;
-
-				resetFish = new List<long>();
+				allFish[8] = resetFish;
 
 				Console.WriteLine($"After day {i}: {allFish[0]}, {allFish[1]}, {allFish[2]}, {allFish[3]}, {allFish[4]}, {allFish[5]}, {allFish[6]}, {allFish[7]}, {allFish[8]}");
 			}
 
 			Console.WriteLine(allFish[0] + allFish[1] + allFish[2] + allFish[3] + allFish[4] + allFish[5] + allFish[6] + 
-				allFish[7] + allFish[8] + resetFish.Count);
+				allFish[7] + allFish[8]);
 
 			Console.ReadLine();
 		}
