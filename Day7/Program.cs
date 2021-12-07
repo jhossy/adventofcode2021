2067e14b38f5d21315498fd5e1227f6f0554b812
Jesper Hossy <JEHOS@danskebank.dk>
2021-12-07 14:01:09 +0100

    added part 2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -16,6 +16,16 @@ namespace Day7
 			int maxValue = puzzleInput.Max();
 			int minValue = puzzleInput.Min();
 
+			Task1(minValue, maxValue, puzzleInput);
+
+			Task2(minValue, maxValue, puzzleInput);
+			
+			Console.WriteLine(FacSum(11));
+			Console.ReadLine();
+		}
+
+		private static int Task1(int minValue, int maxValue, int[] puzzleInput)
+		{
 			int lowestFuelCost = int.MaxValue;
 			int tmpFuelCost = 0;
 			for (int j = minValue; j < maxValue; j++)
@@ -26,14 +36,43 @@ namespace Day7
 					tmpFuelCost += Math.Abs(puzzleInput[i] - j);
 				}
 
-				if(tmpFuelCost < lowestFuelCost)
+				if (tmpFuelCost < lowestFuelCost)
 				{
-					lowestFuelCost = tmpFuelCost;	
+					lowestFuelCost = tmpFuelCost;
 				}
-			}				
-
+			}
 			Console.WriteLine($"Max: {maxValue} - Min:{minValue} - LowestFuelCost: {lowestFuelCost}");
-			Console.ReadLine();
+			return lowestFuelCost;
+		}
+
+		private static int Task2(int minValue, int maxValue, int[] puzzleInput)
+		{
+			int lowestFuelCost = int.MaxValue;
+			int tmpFuelCost = 0;
+			for (int j = minValue; j < maxValue; j++)
+			{
+				tmpFuelCost = 0;
+				for (int i = 0; i < puzzleInput.Length; i++)
+				{
+					tmpFuelCost += FacSum(Math.Abs(puzzleInput[i] - j));
+				}
+
+				if (tmpFuelCost < lowestFuelCost)
+				{
+					lowestFuelCost = tmpFuelCost;
+				}
+			}
+			Console.WriteLine($"Max: {maxValue} - Min:{minValue} - LowestFuelCost: {lowestFuelCost}");
+			return lowestFuelCost;
+		}
+
+		private static int FacSum(int x)
+		{
+			if(x == 0) return 0;
+
+			if (x == 1) return x;
+
+			return x + FacSum(x - 1);
 		}
 	}
 
