f6b4cdb1b0bf1dd07fbd02f980eee192b765ce6f
Jesper Hossy <JEHOS@danskebank.dk>
2021-12-09 14:26:07 +0100

    added day 9 start

---
+++ Day9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day9
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			FileParser fileParser = new FileParser();
+			fileParser.Parse("DemoInput.txt");
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	public class FileParser
+	{
+		public int[,] Parse(string filePath)
+		{
+			int[,] map = new int[0,0];
+
+			if (!File.Exists(filePath)) return map;
+
+			var lines = File.ReadLines(filePath);
+
+			int width = lines.First().Length;
+			int height = lines.Count();
+			map = new int[height, width];
+
+			int j = 0;
+			foreach (var line in lines)
+			{
+				for (int i = 0; i < line.Length; i++)
+				{
+					map[j, i] = int.Parse(line[i].ToString());
+				}
+				j++;
+			}
+
+			return map;
+		}
+	}
+}
